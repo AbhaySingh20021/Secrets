3373c9d45d244fd15beb110660ddea0569a317af
Abhay <Abhaysengar3250@gmail.com>
2021-12-14 21:11:29 +0530

    ini

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const express = require("express");
 const bodyParser= require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-var encrypt = require('mongoose-encryption');
-const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const encrypt = require('mongoose-encryption');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose =require("passport-local-mongoose");
+
+
+//const md5 = require("md5");
+//const bcrypt = require("bcrypt");
+//const saltRounds = 10;
 
 
 
@@ -16,6 +21,11 @@ const app = express();
 app.use(express.static("public"));
 app.set("view engine" , "ejs" );
 mongoose.connect("mongodb://localhost:27017/userDB");
+
+app.use(session({
+    secret: "our little" , resave : false , saveUninitialized : false  }));
+app.use(passport.initialize());
+app.use(passport.session);
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/" , function( req , res) {
@@ -39,67 +49,28 @@ const userScheme = new mongoose.Schema ({
     email: String , password : String
 });
 
-console.log( process.env.SECRET);
+userScheme.plugin(passportLocalMongoose);
 
 
 ///userScheme.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ["password"]});
 
 
 const user = new mongoose.model("User" , userScheme);
+passport.use(user.createStrategy);
+passport.serializeUser(user.serializeUser);
+passport.deserializeUser(user.deserializeUser);
 
 app.post("/register" , function ( req , res){
 
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser = new user ({
-            email: req.body.username , password: hash
-        });
-        newUser.save( function (err) {
-            if(err){
-                console.log(err);
-            }
-            else {
-                res.render("secrets");
-            }
-            
-        } );
-        // Store hash in your password DB.
-    });
-
+    
     
 });
 
 app.post("/login" , function (req, res) {
- const   email= req.body.username; 
- const password = req.body.password;
 
- user.findOne({email : email} , function (err , result) {
-     if(err){
-         console.log(err);
-     }
-         else {
-             if(result){
-                bcrypt.compare(password, result.password, function(err, result1) {
-                    console.log(result1);
-
-                    if(result1 == true ){
-                        res.render("secrets");
-                        console.log("user found");
-                    }
-                    
-
-                   
-                    // result == true
-                });
-                     
-                
-             }
-             else
-             alert("No user found");
-            
-         }
      
-     
- });
+    
+ 
 
 
 
